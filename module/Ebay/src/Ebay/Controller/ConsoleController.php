6fec3122cb8f3073ffc1c787e5618563f341eb25
wizard2014 <wizard2014@ya.ru>
2015-08-13 18:12:47 +0300

    ebay category beta

--- module/Ebay/src/Ebay/Controller/ConsoleController.php
+++ module/Ebay/src/Ebay/Controller/ConsoleController.php
@@ -31,7 +31,7 @@ class ConsoleController extends AbstractActionController
 
         $request = new Types\GetCategoriesRequestType();
         $request->RequesterCredentials = new Types\CustomSecurityHeaderType();
-        $request->RequesterCredentials->eBayAuthToken = 'AgAAAA**AQAAAA**aAAAAA**ezyYVQ**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6AHmIWkAZGCpwqdj6x9nY+seQ**R+kCAA**AAMAAA**YszBHWrBvcPk6CLTKipwXRbHrkSVIMxuud6yMqr7rT5Nt6HLGBNB2lPG1Nq2VQPqlcbRJU8PboY4Zim3ipHO4rNskSAV2o901+e0Md//vc1hMLGfzItvPQeaRAYHOex1jdAkFErLPvUP7eR9/VmhDKzxh24DlgrXla7qhD3gY5PZ9G1IKULzaRUtPxB/m+Rx29roBDuv1ByQkSG8OmlvFruVst5R6ob55XDEnHVa8RidVsWNyepJS5ZvfSHwVF00r0bO7U7PAv45M7sVlG4D6WAXG9yi6fLKo53EyGFFB0kTjG0Ukihatxyl7rDf5Fzzooiq44OZyPatXlEmSV5IQvGdATpQOeqh5NMr6lnAHKEs9zwCoqUXCw101XNwLkGUMj3RTPtcf2kiBXUipwPgUhIDx5pQzBMmPX34aa86VJ4GM8St8J1ACfJdS8IzTz+r+v1ttJKyncQaBFKmAfL4qpTsVd/9zGt01e+t6Q7PnSjfm+jSmX4grGxsxGH4/rAnrgcnjhe+FVL/4D98mOblCbuKExFh8gVkyWURYzypmI2etc/tmUZdsrEghnkBvwKi68VG2VCZLFfFs6pqCtpJasNR/WXNrxQCZc8h3J2VEn1Mf/9hJ5kZPnMatr1iatk7wUQpkMKFb9p2AgUjwbkJ+lslG8RR4AW72vNrljd1iwqZrznTjmqtUNbZLywKdXwnDahJvyawZ8ZUryGH9j6CRjZ6f+vHia6ihs+xJP8AXlxu61e63NJVU/BtqupdfJhd';
+        $request->RequesterCredentials->eBayAuthToken = '';
 
         $request->DetailLevel = ['ReturnAll'];
 
