2b215005a52948fdf2a532325db430d71fbd8c39
wizard2014 <wizard2014@ya.ru>
2015-09-17 23:53:49 +0300

    clear index controller application

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,28 +9,6 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        $em = $this->getServiceLocator()->get(\Doctrine\ORM\EntityManager::class);
-
-        $dataSourceGlobal = $em->find(\Utility\Entity\DataSourceGlobal::class, 1);
-
-        $dataSourceGlobalId = $dataSourceGlobal->getId();
-
-        $regions = $em->getRepository(\Utility\Entity\DataSourceRegional::class)->findAll();
-
-        $result = [];
-
-        foreach ($regions as $region) {
-            $lang = $region->getPropertySet()['ebay_language'];
-            $currentDataSourceGlobalId = $region->getDataSourceGlobal()->getId();
-
-            if (strpos($lang, 'en') !== false && $currentDataSourceGlobalId == $dataSourceGlobalId) {
-                $result[] = $region;
-            }
-        }
-
-        var_dump($result);
-
-
         return new ViewModel();
     }
 }
