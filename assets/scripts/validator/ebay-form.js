672e093b61e29dd843bd11201a22fc0b0de51d4f
wizard2014 <wizard2014@ya.ru>
2015-10-08 17:49:32 +0300

    enable/disable submit button

---
+++ assets/scripts/validator/ebay-form.js
@@ -0,0 +1,30 @@
+// if category exists
+$('.ebay-form').on('submit', function(e) {
+    if (!checkCategoryExists()) {
+        e.preventDefault();
+    }
+});
+
+// enable/disable button
+// when modal hidden
+$('#modal').on('hidden.bs.modal', function() {
+    !checkCategoryExists() ? disableBtn() : enableBtn();
+});
+// when category remove
+$(document).on('click', '.category-remove', function() {
+    setTimeout(function() {
+        !checkCategoryExists() ? disableBtn() : enableBtn();
+    }, 450);
+});
+
+function checkCategoryExists() {
+    return !$('.category-list').is(':empty');
+}
+
+// enable/disable button
+function enableBtn() {
+    $('.submit-btn').removeClass('disabled').prop('disabled', false);
+}
+function disableBtn() {
+    $('.submit-btn').addClass('disabled').prop('disabled', true);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ gulp.task('scripts', function() {
         paths.bower   + '/bootstrap-material-design/dist/js/material.js',
         paths.bower   + '/bootstrap-material-design/dist/js/ripples.js',
         paths.assets  + '/scripts/base/*.js',
+        paths.assets  + '/scripts/validator/*.js',
         paths.assets  + '/scripts/init/*.js'
     ])
         .pipe(concat('app.js'))
